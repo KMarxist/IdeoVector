738fbfbb4889607ca471bbc1f7730af163d2c83b
KMarxist <KMarxist@protonmail.com>
2022-12-18 17:57:51 +0800

    Fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import dns from 'node:dns';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import WindiCSS from 'vite-plugin-windicss';
@@ -9,6 +10,9 @@ export default defineConfig({
   plugins: [WindiCSS(), solidPlugin()],
   build: {
     target: 'chrome59',
+    rollupOptions: {
+      input: { index: resolve(__dirname, 'index.html'), '404': resolve(__dirname, '404.html') },
+    },
   },
   base: '/IdeoVector/',
 });
