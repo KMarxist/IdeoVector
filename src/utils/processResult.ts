c259554d9724c5e670361db42c303fcb6053bb8b
KMarxist <KMarxist@protonmail.com>
2022-12-18 19:35:50 +0800

    Fix: if there's a bug

--- src/utils/processResult.ts
+++ src/utils/processResult.ts
@@ -4,11 +4,11 @@ import { fromByteArray, toByteArray } from 'base64-js';
 export const encResult = (resultAry: number[]) => {
   const gzipped = gzip.zip(resultAry, { level: 9 });
   const base64 = fromByteArray(new Uint8Array(gzipped));
-  return base64;
+  return encodeURIComponent(base64);
 };
 
 export const decResult: (base64: string) => (2 | 1 | 0 | -1 | -2)[] = (base64) => {
-  const gzipped = toByteArray(base64);
+  const gzipped = toByteArray(decodeURIComponent(base64));
   const unzipped = gzip.unzip(gzipped);
   return Array.from(new Int8Array(unzipped)) as (2 | 1 | 0 | -1 | -2)[];
 };
